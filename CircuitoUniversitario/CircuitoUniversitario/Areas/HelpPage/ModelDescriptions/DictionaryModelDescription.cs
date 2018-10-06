df0b634898a912bae92427ea13478c6b25e264d4
TheGrUbA <breynerg@gmail.com>
2018-10-06 20:29:47 -0300

    Criação de repositorio git

---
+++ CircuitoUniversitario/CircuitoUniversitario/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CircuitoUniversitario.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CircuitoUniversitario/CircuitoUniversitario/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace CircuitoUniversitario.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
